50aa5125daff523d3badd4a57902311eb27b46b8
Fauerr <10665994@ucn.dk>
2025-03-31 10:55:28 +0200

    Test 1
    Test

--- TestBLueJ/Class2.java
+++ TestBLueJ/Class2.java
@@ -1,19 +1,19 @@
 
 /**
- * Write a description of class Yooo here.
+ * Write a description of class Class2 here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Yooo
+public class Class2
 {
     // instance variables - replace the example below with your own
     private int x;
 
     /**
-     * Constructor for objects of class Yooo
+     * Constructor for objects of class Class2
      */
-    public Yooo()
+    public Class2()
     {
         // initialise instance variables
         x = 0;
@@ -25,5 +25,9 @@ public class Yooo
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
 }
